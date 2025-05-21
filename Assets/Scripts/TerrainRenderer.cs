2448385081b3fe59e67e315720f3bbe58ae5a4a8
Ansis <ansis.malins@decentraland.org>
2025-05-21 17:26:47 +0200

    Played around with noise

--- Assets/Scripts/TerrainRenderer.cs
+++ Assets/Scripts/TerrainRenderer.cs
@@ -1,1 +1,133 @@
-using System.Collections.Generic;using UnityEngine;using UnityEngine.Rendering;namespace Decentraland.Terrain{    [ExecuteAlways]    public sealed class TerrainRenderer : MonoBehaviour    {        [SerializeField] private Material material;        [SerializeField] private int parcelSize = 16;        [SerializeField] private int terrainSize;        private Mesh parcelMesh;        private RenderParams renderParams;        private Matrix4x4[] instanceData;#if UNITY_EDITOR        public Mesh ParcelMesh => parcelMesh;#endif        private void OnValidate()        {#if UNITY_EDITOR            if (parcelMesh != null)            {                if (!Application.isPlaying)                    Object.DestroyImmediate(parcelMesh);                else                    Object.Destroy(parcelMesh);            }#endif            parcelMesh = CreateParcelMesh(parcelSize);            renderParams.instanceID = GetInstanceID();            renderParams.layer = gameObject.layer;            renderParams.material = material;            renderParams.receiveShadows = true;            renderParams.renderingLayerMask = RenderingLayerMask.defaultRenderingLayerMask;            renderParams.shadowCastingMode = ShadowCastingMode.On;            float terrainHalfSize = parcelSize * terrainSize * 0.5f;            renderParams.worldBounds = new Bounds(                new Vector3(0f, 2f, 0f),                new Vector3(terrainHalfSize, 4f, terrainHalfSize));            instanceData = new Matrix4x4[terrainSize * terrainSize];            for (int z = 0; z < terrainSize; z++)                for (int x = 0; x < terrainSize; x++)                    instanceData[z * terrainSize + x] = Matrix4x4.Translate(                        new Vector3(x * parcelSize - terrainHalfSize, 0f, z * parcelSize - terrainHalfSize));        }        private void OnEnable()        {            RenderPipelineManager.beginContextRendering += DrawParcels;        }        private void OnDisable()        {            RenderPipelineManager.beginContextRendering -= DrawParcels;        }        private void OnDestroy()        {            if (parcelMesh != null)            {#if UNITY_EDITOR                if (!Application.isPlaying)                    Object.DestroyImmediate(parcelMesh);                else#endif                Object.Destroy(parcelMesh);            }        }        private static Mesh CreateParcelMesh(int parcelSize)        {            var parcelMesh = new Mesh()            {                name = "Terrain Parcel",                hideFlags = HideFlags.DontSave            };            int sideVertexCount = parcelSize + 1;            var vertices = new Vector3[sideVertexCount * sideVertexCount];            for (int z = 0; z <= parcelSize; z++)                for (int x = 0; x <= parcelSize; x++)                    vertices[z * sideVertexCount + x] = new Vector3(x, 0f, z);            var triangles = new int[parcelSize * parcelSize * 6];            int index = 0;            for (int z = 0; z < parcelSize; z++)            {                for (int x = 0; x < parcelSize; x++)                {                    int start = z * sideVertexCount + x;                    triangles[index++] = start;                    triangles[index++] = start + sideVertexCount + 1;                    triangles[index++] = start + 1;                    triangles[index++] = start;                    triangles[index++] = start + sideVertexCount;                    triangles[index++] = start + sideVertexCount + 1;                }            }            var normals = new Vector3[sideVertexCount * sideVertexCount];            for (int z = 0; z <= parcelSize; z++)                for (int x = 0; x <= parcelSize; x++)                    normals[z * sideVertexCount + x] = Vector3.up;            parcelMesh.vertices = vertices;            parcelMesh.triangles = triangles;            parcelMesh.normals = normals;            parcelMesh.UploadMeshData(true);            return parcelMesh;        }        private void DrawParcels(ScriptableRenderContext context, List<Camera> cameras)        {            Graphics.RenderMeshInstanced(in renderParams, parcelMesh, 0, instanceData);        }    }}
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Decentraland.Terrain
+{
+    [ExecuteAlways]
+    public sealed class TerrainRenderer : MonoBehaviour
+    {
+        [SerializeField] private Material material;
+        [SerializeField] private int parcelSize = 16;
+        [SerializeField] private int terrainSize;
+
+        private Mesh parcelMesh;
+        private RenderParams renderParams;
+        private Matrix4x4[] instanceData;
+
+#if UNITY_EDITOR
+        public Mesh ParcelMesh => parcelMesh;
+#endif
+
+        private void OnValidate()
+        {
+#if UNITY_EDITOR
+            if (parcelMesh != null)
+            {
+                if (!Application.isPlaying)
+                    Object.DestroyImmediate(parcelMesh);
+                else
+                    Object.Destroy(parcelMesh);
+            }
+#endif
+
+            parcelMesh = CreateParcelMesh(parcelSize);
+
+            renderParams.instanceID = GetInstanceID();
+            renderParams.layer = gameObject.layer;
+            renderParams.material = material;
+            renderParams.receiveShadows = true;
+            renderParams.renderingLayerMask = RenderingLayerMask.defaultRenderingLayerMask;
+            renderParams.shadowCastingMode = ShadowCastingMode.On;
+
+            float terrainHalfSize = parcelSize * terrainSize * 0.5f;
+
+            renderParams.worldBounds = new Bounds(
+                new Vector3(0f, 2f, 0f),
+                new Vector3(terrainHalfSize, 4f, terrainHalfSize));
+
+            instanceData = new Matrix4x4[terrainSize * terrainSize];
+
+            for (int z = 0; z < terrainSize; z++)
+                for (int x = 0; x < terrainSize; x++)
+                    instanceData[z * terrainSize + x] = Matrix4x4.Translate(
+                        new Vector3(x * parcelSize - terrainHalfSize, 0f, z * parcelSize - terrainHalfSize));
+        }
+
+        private void OnEnable()
+        {
+            RenderPipelineManager.beginContextRendering += DrawParcels;
+        }
+
+        private void OnDisable()
+        {
+            RenderPipelineManager.beginContextRendering -= DrawParcels;
+        }
+
+        private void OnDestroy()
+        {
+            if (parcelMesh != null)
+            {
+#if UNITY_EDITOR
+                if (!Application.isPlaying)
+                    Object.DestroyImmediate(parcelMesh);
+                else
+#endif
+                Object.Destroy(parcelMesh);
+            }
+        }
+
+        private static Mesh CreateParcelMesh(int parcelSize)
+        {
+            var parcelMesh = new Mesh()
+            {
+                name = "Terrain Parcel",
+                hideFlags = HideFlags.DontSave
+            };
+
+            int sideVertexCount = parcelSize + 1;
+            var vertices = new Vector3[sideVertexCount * sideVertexCount];
+
+            for (int z = 0; z <= parcelSize; z++)
+                for (int x = 0; x <= parcelSize; x++)
+                    vertices[z * sideVertexCount + x] = new Vector3(x, 0f, z);
+
+            var triangles = new int[parcelSize * parcelSize * 6];
+            int index = 0;
+
+            for (int z = 0; z < parcelSize; z++)
+            {
+                for (int x = 0; x < parcelSize; x++)
+                {
+                    int start = z * sideVertexCount + x;
+
+                    triangles[index++] = start;
+                    triangles[index++] = start + sideVertexCount + 1;
+                    triangles[index++] = start + 1;
+
+                    triangles[index++] = start;
+                    triangles[index++] = start + sideVertexCount;
+                    triangles[index++] = start + sideVertexCount + 1;
+                }
+            }
+
+            var normals = new Vector3[sideVertexCount * sideVertexCount];
+
+            for (int z = 0; z <= parcelSize; z++)
+                for (int x = 0; x <= parcelSize; x++)
+                    normals[z * sideVertexCount + x] = Vector3.up;
+
+            parcelMesh.vertices = vertices;
+            parcelMesh.triangles = triangles;
+            parcelMesh.normals = normals;
+            parcelMesh.UploadMeshData(true);
+
+            return parcelMesh;
+        }
+
+        private void DrawParcels(ScriptableRenderContext context, List<Camera> cameras)
+        {
+            Graphics.RenderMeshInstanced(in renderParams, parcelMesh, 0, instanceData);
+        }
+    }
+}
--- Assets/Shaders/MountainsNoise.hlsl
+++ Assets/Shaders/MountainsNoise.hlsl
@@ -3,72 +3,42 @@
 
 #include "Noise/noise2D.cginc"
 
-void MountainsNoise_float(float2 position, float scale, float2 octave0, float2 octave1, float2 octave2,
-    float maxHeight, float persistence, float lacunarity, float2 offset, float cutoff, float baseValue,
-    float multiplyValue, float divideValue, out float value)
+void MountainsNoise_float(float3 positionIn, float scale, float2 octave0, float2 octave1, float2 octave2,
+    float persistence, float lacunarity, float multiplyValue, out float3 positionOut, out float3 normal)
 {
-    float halfWidth = 0.5;
-    float halfHeight = 0.5;
-
     float amplitude = 1.0;
     float frequency = 1.0;
     float noiseHeight = 0.0;
 
     // Octave 0
     {
-        float sampleX = (position.x - halfWidth + octave0.x + offset.x) / scale * frequency;
-        float sampleY = (position.y - halfHeight + octave0.y + offset.y) / scale * frequency;
-
-        float noiseValue = snoise(float2(sampleX, sampleY));
-
-        noiseValue = (noiseValue * 2.0) - 1.0;
-        noiseHeight += noiseValue * amplitude;
-
+        float2 sample = (positionIn.xz + octave0) * scale * frequency;
+        noiseHeight += snoise(sample) * amplitude;
         amplitude *= persistence;
         frequency *= lacunarity;
     }
 
     // Octave 1
     {
-        float sampleX = (position.x - halfWidth + octave1.x + offset.x) / scale * frequency;
-        float sampleY = (position.y - halfHeight + octave1.y + offset.y) / scale * frequency;
-
-        float noiseValue = snoise(float2(sampleX, sampleY));
-
-        noiseValue = (noiseValue * 2.0) - 1.0;
-        noiseHeight += noiseValue * amplitude;
-
+        float2 sample = (positionIn.xz + octave1) * scale * frequency;
+        noiseHeight += snoise(sample) * amplitude;
         amplitude *= persistence;
         frequency *= lacunarity;
     }
 
     // Octave 2
     {
-        float sampleX = (position.x - halfWidth + octave2.x + offset.x) / scale * frequency;
-        float sampleY = (position.y  - halfHeight + octave2.y + offset.y) / scale * frequency;
-
-        float noiseValue = snoise(float2(sampleX, sampleY));
-
-        noiseValue = (noiseValue * 2.0) - 1.0;
-        noiseHeight += noiseValue * amplitude;
-
+        float2 sample = (positionIn.xz + octave2) * scale * frequency;
+        noiseHeight += snoise(sample) * amplitude;
         amplitude *= persistence;
         frequency *= lacunarity;
     }
 
-    float tempValue = noiseHeight;
+    positionOut = positionIn;
+    positionOut.y += noiseHeight * multiplyValue;
 
-    tempValue += baseValue;
-    tempValue *= max(multiplyValue, 1.0);
-    tempValue /= max(divideValue, 1.0);
-
-    float normalizedHeight = (tempValue + 1.0) / maxHeight;
-    tempValue = clamp(normalizedHeight, 0.0, 1.0);
-
-    if (tempValue < cutoff)
-        tempValue = 0.0;
-
-    value = tempValue;
+    // TODO: Generate noise with derivatives, and use derivatives to generate normals.
+    normal = float3(0.0, 1.0, 0.0);
 }
 
 #endif
