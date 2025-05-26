445a7a288d525f9bd3186cd0d3b4915f69dda8e1
Geoff Birch <geoff.birch@decentraland.org>
2025-05-26 13:38:43 +0100

    Start of replacement of Mountain Shader

--- Assets/Shaders/MountainsNoise.hlsl
+++ Assets/Shaders/MountainsNoise.hlsl
@@ -41,4 +41,37 @@ void MountainsNoise_float(float3 positionIn, float scale, float2 octave0, float2
     normal = float3(0.0, 1.0, 0.0);
 }
 
+void TerrainNormalDerivatives_float(float3 worldPos, float _NormalScale, out float4 oTangentNormal)
+{
+    float3 dFdxPos = ddx(worldPos.xyz);
+    float3 dFdyPos = ddy(worldPos.xyz);
+
+    // Calculate world normal
+    float3 worldNormal = normalize(cross(dFdyPos, dFdxPos));
+
+    // Construct tangent space basis from screen-space derivatives
+    // dFdx gives us one tangent direction, dFdy gives us another
+    float3 tangent = normalize(dFdxPos);
+    float3 bitangent = normalize(dFdyPos);
+
+    // Ensure orthogonality using Gram-Schmidt process
+    bitangent = normalize(bitangent - dot(bitangent, tangent) * tangent);
+
+    // Create world-to-tangent matrix
+    float3x3 worldToTangent = float3x3(
+        tangent.x, bitangent.x, worldNormal.x,
+        tangent.y, bitangent.y, worldNormal.y,
+        tangent.z, bitangent.z, worldNormal.z
+    );
+
+    // Transform world normal to tangent space
+    float3 tangentNormal = mul(worldToTangent, worldNormal);
+    tangentNormal = normalize(tangentNormal) * _NormalScale;
+
+    // Convert to [0,1] range for output
+    float3 normalOutput = tangentNormal * 0.5 + 0.5;
+
+    oTangentNormal = float4(normalOutput, 1.0);
+}
+
 #endif
