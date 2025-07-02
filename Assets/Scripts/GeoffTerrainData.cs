f0671a94c947f58f1f5ebb043d4c8dcba39d4541
Ansis <ansis.malins@decentraland.org>
2025-07-02 12:20:25 +0200

    Fix Geoff's terrain data

---
+++ Assets/Scripts/GeoffTerrainData.cs
@@ -0,0 +1,27 @@
+﻿using AOT;
+using Decentraland.Terrain;
+using Unity.Burst;
+using Unity.Mathematics;
+using UnityEngine;
+using TerrainData = Decentraland.Terrain.TerrainData;
+
+namespace TerrainProto
+{
+    [BurstCompile, CreateAssetMenu]
+    public class GeoffTerrainData : TerrainData
+    {
+        protected override void CompileNoiseFunctions()
+        {
+            getHeight = BurstCompiler.CompileFunctionPointer(new GetHeightDelegate(GetHeight));
+            getNormal = BurstCompiler.CompileFunctionPointer(new GetNormalDelegate(GetNormal));
+        }
+
+        [BurstCompile, MonoPInvokeCallback(typeof(GetHeightDelegate))]
+        private static float GetHeight(float x, float z) =>
+            GeoffNoise.GetHeight(x, z);
+
+        [BurstCompile, MonoPInvokeCallback(typeof(GetNormalDelegate))]
+        private static void GetNormal(float x, float z, out float3 normal) =>
+            normal = GeoffNoise.GetNormal(x, z);
+    }
+}
